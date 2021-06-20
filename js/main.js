f6691ce322f519c221dbf44d3a657a499e2ea3b6
Muhammed Munees <muneesmmm@gmail.com>
2021-06-20 13:26:09 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,83 @@
+(function($) {
+
+	"use strict";
+
+
+  // Form
+	var contactForm = function() {
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					name: "required",
+					phone: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					message: {
+						url:"https://wa.me/9645732562?text=How%20are%20you%20?"
+					}
+				},
+				messages: {
+					name: "Please enter your name",
+					phonr: "Please enter your phonr number",
+					email: "Please enter a valid email address",
+				},
+				/* submit via ajax */
+				
+				submitHandler: function(form) {		
+					var $submit = $('.submitting'),
+						waitText = 'Submitting...';
+
+					$.ajax({   	
+				      type: "POST",
+				      url: "https://script.google.com/macros/s/AKfycbwFtOOCpG02GYoC3WQKy_h00xEhIbQOku8rRlr-znDhEAonUi4EHlWQmDYKJQgVpGg3/exec",
+				      data: $(form).serialize(),
+
+				      beforeSend: function() { 
+				      	$submit.css('display', 'block').text(waitText);
+				      },
+				      success: function(msg) {
+		               if (msg == 'OK') {
+		               	$('#form-message-warning').hide();
+				            setTimeout(function(){
+		               		$('#contactForm').fadeIn();
+		               	}, 1000);
+				            setTimeout(function(){
+				               $('#form-message-success').fadeIn();   
+		               	}, 1400);
+
+		               	setTimeout(function(){
+				               $('#form-message-success').fadeOut();   
+		               	}, 8000);
+
+		               	setTimeout(function(){
+				               $submit.css('display', 'none').text(waitText);  
+		               	}, 1400);
+
+		               	setTimeout(function(){
+		               		$( '#contactForm' ).each(function(){
+											    this.reset();
+											});
+		               	}, 1400);
+			               
+			            } else {
+			               $('#form-message-warning').html(msg);
+				            $('#form-message-warning').fadeIn();
+				            $submit.css('display', 'none');
+			            }
+				      },
+				      error: function() {
+				      	$('#form-message-warning').html("Something went wrong. Please try again.");
+				         $('#form-message-warning').fadeIn();
+				         $submit.css('display', 'none');
+				      }
+			      });    		
+		  		} // end submitHandler
+
+			});
+		}
+	};
+	contactForm();
+
+})(jQuery);
